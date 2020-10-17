35d5272c3a3d7c2f232c74267bd2c404ba46e716
unknown <Administrator@F4T2.SVITVASAD.LOCAL>
2020-10-17 16:24:24 +0530

    intial commit

---
+++ app/blog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use League\CommonMark\Extension\Table\Table;
+
+class blog extends Model
+{
+    //
+    protected $table = 'blog';
+}
